bbd1bd831ac157c5c3548ae8d03b39b660420126
LuAndre49 <134673214+LuAndre49@users.noreply.github.com>
2024-06-30 23:13:07 +0800

    setup variables and arrays

--- Code/idft.c
+++ Code/idft.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+
+void idft(float* xr, float* xi) {
+	int N = sizeof(xr)/sizeof(xr[0]);
+	float* x = (float*)malloc(N * sizeof(float));
+	
+}
 
 int main() {
-	
+	int N = 10;
+	float xr[10], xi[10];
+
+	for (int i = 0; i < N; i++){
+		xr[i] = (float)i;
+		xi[i] = (float)(i*2);
+	}	
+
+	idft(&xr, &xi);
 
 	return 0;
 }
