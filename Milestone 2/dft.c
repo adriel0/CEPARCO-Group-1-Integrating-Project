8358b1c54406eedb68880741dbcc96637612da18
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2024-07-05 20:57:17 +0800

    Update dft.c

--- Milestone 2/dft.c
+++ Milestone 2/dft.c
@@ -7,18 +7,17 @@
 void dft(float* x, int N) {
 	float* xr = (float*)malloc(N * sizeof(float));
 	float* xi = (float*)malloc(N * sizeof(float));
-	int k, n;
 	float theta;
 
-	for (k = 0; k < N; k++) {
+	for (int k = 0; k < N; k++) {
 		xr[k] = 0;
 		xi[k] = 0;
-		for (n = 0; n < N; n++) {
+		for (int n = 0; n < N; n++) {
 			theta = (2 * PI * k * n) / N;
 			xr[k] = xr[k] + x[n] * cos(theta);
 			xi[k] = xi[k] - x[n] * sin(theta);
 		}
-		printf("%f + j(%f)\n", xr[k], xi[k]);
+		printf("%.3f + j(%.3f)\n", xr[k], xi[k]);
 	}
 
 	free(xr);
@@ -36,6 +35,7 @@ int main() {
 	}
 
 	dft(x, N);
+	
 	free(x);
 	return 0;
 }
