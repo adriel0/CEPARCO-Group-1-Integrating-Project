87fd81812cdbb780306a63ef05064bc15d40e151
LuAndre49 <134673214+LuAndre49@users.noreply.github.com>
2024-07-05 19:01:31 +0800

    adjusted formula in idft

--- Milestone 2/idft.c
+++ Milestone 2/idft.c
@@ -4,25 +4,26 @@
 
 #define PI 3.14159265358979323846
 
-void idft(float* xr, float* xi) {
-	int N = sizeof(xr)/sizeof(xr[0]);
+void idft(float N, float* xr, float* xi) {
 	float* y = (float*)malloc(N * sizeof(float));
 	float theta;
 	
+	printf("IDFT:\n");
 	for (int n = 0; n < N; n++){
+		y[n] = 0;
 		for (int k = 0; k < N; k++){
 			theta = (2 * PI * k * n)/N;
-			y[n] = y[n] + xr[k] * cos(theta) + xi[k] * sin(theta);
+			y[n] = y[n] + xr[k] * cos(theta) - xi[k] * sin(theta);
 		}
 		y[n] = y[n]/N;
-		printf("%f\n", y[n]);
+		printf("y[%d] = %.2f\n", n, y[n]);
 	}
 
 	free(y);
 }
 
 int main() {
-	int N = 10;
+	float N = 10;
 	float* xr = (float*)malloc(N * sizeof(float));
 	float* xi = (float*)malloc(N * sizeof(float));
 	
@@ -31,7 +32,7 @@ int main() {
 		xi[i] = (float)(i*2);
 	}	
 
-	idft(xr, xi);
+	idft(N, xr, xi);
 
 	return 0;
 }
