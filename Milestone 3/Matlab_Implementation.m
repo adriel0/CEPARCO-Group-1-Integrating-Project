311bbd14c0f64f2e6fd8c8fb4fdb3c98e79cfa88
adriel0 <66862486+adriel0@users.noreply.github.com>
2024-07-30 16:50:06 +0800

    Update Matlab_Implementation.m

--- Milestone 3/Matlab_Implementation.m
+++ Milestone 3/Matlab_Implementation.m
@@ -3,19 +3,19 @@
 N = 2^10;
 fprintf("Number of Elements: %d\n", N);
 x = zeros(N, 1);
-y = zeros(N, 1);
 for i = 0:N-1
     x(i+1) = i;
 end
-
-xr = zeros(N, 1);
-xi = zeros(N, 1);
-total_time = 0;
-
-%% calcs for DFT
 loop = 1;   % change to 1 for correctness checking
 fprintf("Number of Loops: %d\n", loop);
+
+
+%% calcs for DFT
+
+total_time = 0;
 for i = 1:loop
+    xr = zeros(N, 1);
+    xi = zeros(N, 1);
     t0 = datetime("now");
     for k = 0:1:(size(x)-1)
         for n = 0:1:(size(x)-1)
@@ -27,15 +27,28 @@ for i = 1:loop
     t1 = datetime("now");
     ms = milliseconds(t1 - t0);
     total_time = total_time + ms;
-    xr = zeros(N, 1);
-    xi = zeros(N, 1);
 end
 average_time = total_time/5;
+
+fprintf('Average time elapsed (DFT): %fms\n', average_time)
+
+total_time = 0;
+for i = 1:loop
+    t0 = datetime("now");
+    temp = fft(x);
+    t1 = datetime("now");
+    bixr = real(temp); %bi for built-in
+    bixi = imag(temp); %bi for built-in
+    ms = milliseconds(t1 - t0);
+    total_time = total_time + ms;
+end
+average_time = total_time/5;
+
 
 %for i = 1:size(x)
 %   fprintf("%f + j(%f)\n", xr(i), xi(i))
 %end
-fprintf('Average time elapsed (DFT): %fms\n', average_time)
+fprintf('Average time elapsed (DFT built-in): %fms\n', average_time)
 
 
 %% output results for DFT correction checking, used for 2^10 for fast output
@@ -43,11 +56,15 @@ fprintf('Average time elapsed (DFT): %fms\n', average_time)
 % for i = 1:1:size(x)
 %     fprintf(fileID,'%f %f\n',xr(i),xi(i));
 % end
+% for i = 1:1:size(x)
+%     fprintf(fileID,'%f %f\n',bixr(i),bixi(i));
+% end
 % fclose(fileID);
 
 %% calcs for IDFT
 total_time = 0;
 for i = 1:loop
+    y = zeros(N, 1);
     t0 = datetime("now");
     for k = 0:1:(size(x)-1)
         for n = 0:1:(size(x)-1)
@@ -59,11 +76,22 @@ for i = 1:loop
     t1 = datetime("now");
     ms = milliseconds(t1 - t0);
     total_time = total_time + ms;
-    y = zeros(N, 1);
 end
 average_time = total_time/5;
 
 %for i = 1:size(x)
-%   fprintf("%f + j(%f)\n", xr(i), xi(i))
+%   fprintf("%f\n", y(i))
 %end
+fprintf('Average time elapsed (IDFT): %fms\n', average_time)
+
+total_time = 0;
+for i = 1:loop
+    y = zeros(N, 1);
+    t0 = datetime("now");
+    biy = ifft(temp); %bi for built-in
+    t1 = datetime("now");
+    ms = milliseconds(t1 - t0);
+    total_time = total_time + ms;
+end
+average_time = total_time/5;
 fprintf('Average time elapsed (IDFT): %fms\n', average_time)
