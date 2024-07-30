d9a885fb0c2b0a829988830373ff3395d9de831b
ELC1123 <115350015+ELC1123@users.noreply.github.com>
2024-07-30 12:17:33 +0800

    Update Matlab_Implementation.m

--- Milestone 3/Matlab_Implementation.m
+++ Milestone 3/Matlab_Implementation.m
@@ -1,6 +1,6 @@
 %%---------------- MATLAB IMPLEMENTATION ---------------------
 %% value initialization
-N = 2^16;
+N = 2^17;
 fprintf("Number of Elements: %d\n", N);
 x = zeros(N, 1);
 y = zeros(N, 1);
@@ -10,46 +10,49 @@ end
 
 xr = zeros(N, 1);
 xi = zeros(N, 1);
+total_time = 0;
 
-%% setting up clock
-t0 = datetime("now");
-
-%% calcs
-for k = 0:1:(size(x)-1)
-    for n = 0:1:(size(x)-1)
-        theta = (2 * pi * k * n)/N;
-        xr(k+1) = xr(k+1) + (x(n+1) * cos(theta));
-        xi(k+1) = xi(k+1) - (x(n+1) * sin(theta));
+%% calcs for DFT
+loop = 1;
+for i = 1:loop
+    t0 = datetime("now");
+    for k = 0:1:(size(x)-1)
+        for n = 0:1:(size(x)-1)
+            theta = (2 * pi * k * n)/N;
+            xr(k+1) = xr(k+1) + (x(n+1) * cos(theta));
+            xi(k+1) = xi(k+1) - (x(n+1) * sin(theta));
+        end
     end
+    t1 = datetime("now");
+    ms = milliseconds(t1 - t0);
+    total_time = total_time + ms;
 end
-
-%% time after calcs
-t1 = datetime("now");
-ms = milliseconds(t1 - t0);
+average_time = total_time/5;
 
 %for i = 1:size(x)
 %   fprintf("%f + j(%f)\n", xr(i), xi(i))
 %end
-fprintf('Time elapsed (DFT): %fms\n', ms)
+fprintf('Average time elapsed (DFT): %fms\n', average_time)
 
 
-%% setting up clock
-t0 = datetime("now");
-
-%% calcs
-for k = 0:1:(size(x)-1)
-    for n = 0:1:(size(x)-1)
-        theta = (2 * pi * k * n)/N;
-        y(k+1) = y(k+1)+ xr(k+1)  * cos(theta) - xi(k+1)  * sin(theta);
+%% calcs for IDFT
+total_time = 0;
+for i = 1:loop
+    t0 = datetime("now");
+    for k = 0:1:(size(x)-1)
+        for n = 0:1:(size(x)-1)
+            theta = (2 * pi * k * n)/N;
+            y(k+1) = y(k+1)+ xr(k+1)  * cos(theta) - xi(k+1)  * sin(theta);
+        end
+        y(k+1) = y(k+1)/N;
     end
-    y(k+1) = y(k+1)/N;
+    t1 = datetime("now");
+    ms = milliseconds(t1 - t0);
+    total_time = total_time + ms;
 end
-
-%% time after calcs
-t1 = datetime("now");
-ms = milliseconds(t1 - t0);
+average_time = total_time/5;
 
 %for i = 1:size(x)
 %   fprintf("%f + j(%f)\n", xr(i), xi(i))
 %end
-fprintf('Time elapsed (IDFT): %fms\n', ms)
+fprintf('Average time elapsed (IDFT): %fms\n', average_time)
